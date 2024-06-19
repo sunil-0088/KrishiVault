a17c785d42e61acfb06d6a748f41b3d5de20d195
sunil-0088 <sunilnagar0088@gmail.com>
2024-06-20 00:41:06 +0530

    add news page

---
+++ src/app/screens/news/components/news-carousel/news-carousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-carousel',
+  templateUrl: './news-carousel.component.html',
+  styleUrls: ['./news-carousel.component.scss']
+})
+export class NewsCarouselComponent {
+
+}
--- src/app/screens/news/news.module.ts
+++ src/app/screens/news/news.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsComponent } from './news.component';
 import { NewsCardComponent } from './components/news-card/news-card.component';
+import { NewsCarouselComponent } from './components/news-carousel/news-carousel.component';
 
 
 
 @NgModule({
   declarations: [
     NewsComponent,
-    NewsCardComponent
+    NewsCardComponent,
+    NewsCarouselComponent
   ],
   imports: [
     CommonModule
