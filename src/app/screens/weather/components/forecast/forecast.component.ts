ebb3ef72307711acd39d544910920e3ba8a23b9e
sunil-0088 <sunilnagar0088@gmail.com>
2024-06-23 23:38:05 +0530

    add graph

--- src/app/screens/weather/components/forecast/forecast.component.ts
+++ src/app/screens/weather/components/forecast/forecast.component.ts
@@ -7,15 +7,20 @@ import {
   ViewChild,
 } from '@angular/core';
 import { ForecastDay } from '../../model/forecast';
+import { Chart, Tooltip } from 'chart.js';
+import { HourData } from '../../model/hour-data';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.scss'],
 })
-export class ForecastComponent implements OnInit {
-  @Input()
-  forecastDays: ForecastDay[] | undefined;
+export class ForecastComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    const ctx = this.chartRef!.nativeElement.getContext('2d');
+    this.initializeChart(ctx);
+  }
+  @ViewChild('myChart') chartRef: ElementRef | undefined;
   times: string[] = [
     '12:00 AM',
     '2:00 AM',
@@ -31,149 +36,81 @@ export class ForecastComponent implements OnInit {
     '10:00 PM',
     '11:00 PM',
   ];
-  basicData: any;
-  basicOptions: any;
-  constructor(
-  ) {}
-  ngOnInit() {
-    this.basicData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: '2020',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          tension: 0.4,
-          borderColor: '#42A5F5',
-        },
-        {
-          label: '2021',
-          data: [28, 48, 40, 19, 86, 27, 90],
-          fill: false,
-          borderDash: [5, 5],
-          tension: 0,
-          borderColor: '#66BB6A',
-        },
-        {
-          label: '2022',
-          data: [12, 51, 62, 33, 21, 62, 45],
-          fill: true,
-          borderColor: '#FFA726',
-          tension: 0.4,
-          backgroundColor: 'rgba(255,167,38,0.2)',
-        },
-      ],
-    };
-    this.basicOptions = {
-      title: {
-        display: true,
-        text: 'Article Views',
-        fontSize: 32,
-        position: 'top',
+  @Input()
+  forecastDays: ForecastDay[] | undefined;
+
+  ngOnInit(): void {
+    // console.log(this.forecastDays);
+  }
+  convertTime(num: number): string {
+    if (num === 0) {
+      return '12:00 AM';
+    } else if (num === 12) {
+      return '12:00 PM';
+    } else if (num > 12) {
+      return `${num - 12}:00 PM`;
+    } else {
+      return `${num}:00 AM`;
+    }
+  }
+  initializeChart(ctx: any) {
+    const times: string[] = [];
+    const temps: number[] = [];
+    const hourData: number[] = [];
+
+    this.forecastDays![0].hour.map((data, index) => {
+      if (index % 2 == 0) {
+        times.push(this.convertTime(index));
+        temps.push(data.temp_c);
+        hourData.push(data.chance_of_rain);
+      }
+    });
+    const chartOptions = {};
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: times,
+        datasets: [
+          {
+            label: 'Temperature (°C)',
+            data: temps,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            yAxisID: 'y-axis-temp',
+            borderWidth: 1,
+          },
+          {
+            label: 'Rain %',
+            data: hourData,
+            borderColor: 'rgb(161, 235, 91,1)',
+            backgroundColor: 'rgb(161, 235, 91,0.2)',
+            yAxisID: 'y-axis-Rain',
+            borderWidth: 1,
+          },
+        ],
       },
-      scales: {
-        x: {
-          ticks: {
-            color: '#495057',
-          },
-          grid: {
-            color: '#ebedef',
+      options: {
+        plugins: {},
+        scales: {
+          // y: {
+          //   title: {
+          //     display:true,
+          //     text: 'Temperature (°C)',
+          //     align: 'center',
+          //   },
+          // },
+          x: {
+            title: {
+              display: true,
+              text: 'Time',
+              align: 'center',
+            },
           },
         },
-        y: {
-          ticks: {
-            color: '#495057',
-          },
-          grid: {
-            color: '#ebedef',
-          },
-        },
+
+        responsive: true,
       },
-    };
+    });
   }
 }
-
-// convertTime(num: number): string {
-//     if (num === 0) {
-//       return '12:00 AM';
-//     } else if (num === 12) {
-//       return '12:00 PM';
-//     } else if (num > 12) {
-//       return `${num - 12}:00 PM`;
-//     } else {
-//       return `${num}:00 AM`;
-//     }
-//   }
-//   initializeChart() {
-//     // const WeatherData = this.forecastDays![0].hour.filter(
-//     //   (_, index) => index % 2 === 0
-//     // );
-//     const times: string[] = [];
-//     const temps: number[] = [];
-//     const hourData = [];
-
-//     this.forecastDays![0].hour.map((data, index) => {
-//       if (index % 2 == 0) times.push(this.convertTime(index));
-//       temps.push(data.temp_c);
-//     });
-
-//     this.chartOptions = {
-//       type: 'line',
-//       data: {
-//         labels: times,
-//         datasets: [
-//           {
-//             label: 'Temperature (°C)',
-//             data: temps,
-//             borderColor: 'rgba(75, 192, 192, 1)',
-//             backgroundColor: 'rgba(75, 192, 192, 0.2)',
-//             fill: false,
-//             yAxisID: 'y-axis-temp',
-//           },
-//           // {
-//           //   label: 'Rain (%)',
-//           //   data: rains,
-//           //   borderColor: 'rgba(54, 162, 235, 1)',
-//           //   backgroundColor: 'rgba(54, 162, 235, 0.2)',
-//           //   fill: false,
-//           //   yAxisID: 'y-axis-rain'
-//           // }
-//         ],
-//       },
-//       options: {
-//         scales: {
-//           yAxes: [
-//             {
-//               id: 'y-axis-temp',
-//               type: 'linear',
-//               position: 'left',
-//               ticks: {
-//                 beginAtZero: true,
-//               },
-//               scaleLabel: {
-//                 display: true,
-//                 labelString: 'Temperature (°C)',
-//               },
-//             },
-//             {
-//               id: 'y-axis-rain',
-//               type: 'linear',
-//               position: 'right',
-//               ticks: {
-//                 beginAtZero: true,
-//               },
-//               scaleLabel: {
-//                 display: true,
-//                 labelString: 'Rain (%)',
-//               },
-//             },
-//           ],
-//         },
-//         responsive: true,
-//         title: {
-//           display: true,
-//           text: 'Temperature and Rain Percentage over Time',
-//         },
-//       },
-//     };
-//   }
