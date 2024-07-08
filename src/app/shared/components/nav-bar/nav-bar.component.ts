e73b8938beb0113c60c7a0919e29ebfe6d94c54c
sunil_kumar <sunil.k@qburst.com>
2024-07-08 15:57:37 +0530

    update

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { User } from 'src/app/screens/auth/modal/user';
 import { AuthService } from 'src/app/screens/auth/services/auth.service';
 
 @Component({
@@ -6,10 +8,28 @@ import { AuthService } from 'src/app/screens/auth/services/auth.service';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent {
-  constructor(private authService: AuthService) {}
+export class NavBarComponent implements OnInit {
+  user:User | undefined | null;
+  isAuthRouteActive: boolean = false;
+
+  constructor(private authService: AuthService,private router: Router) {}
+  ngOnInit(): void {
+     this.authService.user$.subscribe((data)=>{
+      this.user=data;
+     });
+     this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isAuthRouteActive = this.isAuthRoute();
+      }
+    });
+  }
 
   async logOut() {
     await this.authService.signOut();
+  }
+
+
+  isAuthRoute(): boolean {
+    return this.router.url.startsWith('/auth');
   }
 }
